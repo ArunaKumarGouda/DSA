521b24e49fdbefa7b6eeed0ad721aa315aec62a9
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-11 23:44:54 +0530

    Time: 1293 ms (5.22%), Space: 47 MB (98.16%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2];
+
+        int duplicate = 0;
+        int original = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(nums[i] == nums[j]) {
+                    duplicate = nums[i];
+                }
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            boolean found = false;
+            for(int j = 0; j < n; j++) {
+                if(nums[j] == i) {
+                    found = true;
+                }
+            }
+
+            if(found == false) {
+                original = i;
+            }
+        }
+        ans[0] = duplicate;
+        ans[1] = original;
+        return ans;
+    }
+}
