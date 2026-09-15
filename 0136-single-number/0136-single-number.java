7af4da48b14d6c4ab5d304ea7588b13e00a36220
Aruna Kumar Gouda <arunakumargouda86@gmail.com>
2026-09-16 00:41:13 +0530

    Time: 1 ms (99.94%), Space: 47 MB (46.11%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+
+        int number = 0;
+        for(int i = 0; i < n; i++) {
+            number ^= nums[i];
+        }
+
+        return number;
+    }
+}
